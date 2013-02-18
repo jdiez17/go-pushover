9909e76519b433780443647c3ffba49d79801c32
jdiez17 <jose.manuel.diez@gmail.com>
2013-02-18 15:19:36 +0100

    Better API

--- pushover.go
+++ pushover.go
@@ -1,43 +1,64 @@
 package pushover
 
 import (
-    "net/http"
-    "net/url"
-    "io/ioutil"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
 )
 
 const endpoint string = "https://api.pushover.net/1/messages.json"
 
-type Pushover struct { 
-    User, Apikey string
+type Pushover struct {
+	User, Apikey string
 }
 
 type Response struct {
-    Status float64
-    Errors []interface{} 
-    Message string
+	Status  float64
+	Errors  []interface{}
+	Message string
 }
 
-func (p Pushover) Notify(title, message string) (*Response, error) {
-    resp, err := http.PostForm(endpoint, 
-        url.Values{"token": {p.Apikey}, "user": {p.User}, "message": {message}})
+type Notification struct {
+	Message, Title, Url, UrlTitle, Sound, Device string
+	Timestamp                                    time.Time
+	Priority                                     float64
+}
 
-    if err != nil {
-        return nil, err
-    }
+func (n Notification) toValues(p Pushover) url.Values {
+	return url.Values{
+		"token":     {p.Apikey},
+		"user":      {p.User},
+		"message":   {n.Message},
+		"title":     {n.Title},
+		"url":       {n.Url},
+		"url_title": {n.UrlTitle},
+		"sound":     {n.Sound},
+		"device":    {n.Device},
+		"timestamp": {fmt.Sprintf("%d", n.Timestamp.Unix())},
+	}
+}
 
-    if resp.StatusCode == 200 {
-        return nil, nil
-    }
-    defer resp.Body.Close()
-    
-    body, err := ioutil.ReadAll(resp.Body) 
-    if err != nil {
-        return nil, err
-    }
-   
-    response := new(Response) 
-    err = json.Unmarshal(body, response)
-    return response, err
+func (p Pushover) Notify(n Notification) (*Response, error) {
+	resp, err := http.PostForm(endpoint, n.toValues(p))
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode == 200 {
+		return nil, nil
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	response := new(Response)
+	err = json.Unmarshal(body, response)
+	return response, err
 }
